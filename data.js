ce140306a881f699e54844d3bd1e78919a95b725
lichen14 <380120970@qq.com>
2021-08-31 23:02:10 +0800

    Update data.js

--- data.js
+++ data.js
@@ -954,229 +954,229 @@ const activities = {
 //     organizer: [
 //         "Lead organizer for <a href='https://ribfrac.grand-challenge.org/'>MICCAI 2020 RibFrac Challenge: Rib Fracture Detection and Classification</a>."
 //     ],
-//     conference: [
-//         {
-//             venue: "Advances in Neural Information Processing Systems",
-//             year: "2021",
-//             abbr: "NeurIPS"
-//         },
-//         {
-//             venue: "International Conference on Computer Vision",
-//             year: "2021",
-//             abbr: "ICCV"
-//         },
-//         {
-//             venue: "International Conference on Machine Learning",
-//             year: "2021",
-//             abbr: "ICML"
-//         },
-//         {
-//             venue: "International Conference on Medical Image Computing and Computer Assisted Intervention",
-//             year: "2021",
-//             abbr: "MICCAI"
-//         },
-//         {
-//             venue: "IEEE International Symposium on Biomedical Imaging",
-//             year: "2021",
-//             abbr: "ISBI"
-//         },
-//         {
-//             venue: "IEEE Conference on Computer Vision and Pattern Recognition",
-//             year: "2021",
-//             abbr: "CVPR"
-//         },
-//         {
-//             venue: "IEEE Winter Conference on Applications of Computer Vision",
-//             year: "2021",
-//             abbr: "WACV"
-//         },
-//         {
-//             venue: "Asian Conference on Computer Vision",
-//             year: "2020",
-//             abbr: "ACCV"
-//         },
-//         {
-//             venue: "Advances in Neural Information Processing Systems",
-//             year: "2020",
-//             abbr: "NeurIPS"
-//         },
-//         {
-//             venue: "British Machine Vision Conference",
-//             year: "2020",
-//             abbr: "BMVC"
-//         },
-//         {
-//             venue: "International Conference on Medical Image Computing and Computer Assisted Intervention",
-//             year: "2020",
-//             abbr: "MICCAI"
-//         },
-//         {
-//             venue: "IEEE Conference on Computer Vision and Pattern Recognition",
-//             year: "2020",
-//             abbr: "CVPR"
-//         }
-//     ],
-//     journal: [
-//         {
-//             venue: "IEEE Transactions on Medical Imaging",
-//             abbr: "IEEE TMI"
-//         },
-//         {
-//             venue: "IEEE Transactions on Information Forensics and Security",
-//             abbr: "IEEE TIFS"
-//         },
-//         {
-//             venue: "Applied Science",
-//             abbr: "App Sci"
-//         },
-//         {
-//             venue: "IEEE Access"
-//         },
-//         {
-//             venue: "Biomedical Signal Processing and Control"
-//         }
-//     ]
+    conference: [
+        {
+            venue: "Advances in Neural Information Processing Systems",
+            year: "2021",
+            abbr: "NeurIPS"
+        },
+        {
+            venue: "International Conference on Computer Vision",
+            year: "2021",
+            abbr: "ICCV"
+        },
+        {
+            venue: "International Conference on Machine Learning",
+            year: "2021",
+            abbr: "ICML"
+        },
+        {
+            venue: "International Conference on Medical Image Computing and Computer Assisted Intervention",
+            year: "2021",
+            abbr: "MICCAI"
+        },
+        {
+            venue: "IEEE International Symposium on Biomedical Imaging",
+            year: "2021",
+            abbr: "ISBI"
+        },
+        {
+            venue: "IEEE Conference on Computer Vision and Pattern Recognition",
+            year: "2021",
+            abbr: "CVPR"
+        },
+        {
+            venue: "IEEE Winter Conference on Applications of Computer Vision",
+            year: "2021",
+            abbr: "WACV"
+        },
+        {
+            venue: "Asian Conference on Computer Vision",
+            year: "2020",
+            abbr: "ACCV"
+        },
+        {
+            venue: "Advances in Neural Information Processing Systems",
+            year: "2020",
+            abbr: "NeurIPS"
+        },
+        {
+            venue: "British Machine Vision Conference",
+            year: "2020",
+            abbr: "BMVC"
+        },
+        {
+            venue: "International Conference on Medical Image Computing and Computer Assisted Intervention",
+            year: "2020",
+            abbr: "MICCAI"
+        },
+        {
+            venue: "IEEE Conference on Computer Vision and Pattern Recognition",
+            year: "2020",
+            abbr: "CVPR"
+        }
+    ],
+    journal: [
+        {
+            venue: "IEEE Transactions on Medical Imaging",
+            abbr: "IEEE TMI"
+        },
+        {
+            venue: "IEEE Transactions on Information Forensics and Security",
+            abbr: "IEEE TIFS"
+        },
+        {
+            venue: "Applied Science",
+            abbr: "App Sci"
+        },
+        {
+            venue: "IEEE Access"
+        },
+        {
+            venue: "Biomedical Signal Processing and Control"
+        }
+    ]
 };
 
 const talks = [
-//     {
-//         title: "TBD",
-//         materials: [
-//             {
-//                 tag: "slides",
-//                 href: "https://docs.qq.com/pdf/DS1Bab21waEhtdXNF"
-//             },
-//             {
-//                 tag: "video (YouTube)",
-//                 href: "https://www.nova77.org/recorded-video-for-webinar-slides-for-medmnist-classification-decathlon-and-clinical-ai-by-jiancheng-yang/4068/"
-//             }
-//         ],
-//         turns: [
-//             {
-//                 mmyyyy: "02/2021",
-//                 where: " Pie & AI by <a href='https://deeplearning.ai/' target='_blank'>DeepLearning.AI</a> (online)",
-//                 note: "Host: <a href='https://www.eventbrite.com/e/pie-ai-henderson-medmnist-classification-decathlon-clinical-ai-tickets-137316189419#' target='_blank'>Nova77 STEM Workshop</a>"
-//             }
-//         ]
-//     }
-//     {
-//         title: "How to Develop Open Research Dataset: Examples of Medical Images",
-//         materials: [
-//             {
-//                 tag: "slides",
-//                 href: "https://docs.qq.com/pdf/DS0xDdEVwTkd2RnBW"
-//             },
-//             {
-//                 tag: "video (in Chinese)",
-//                 href: "https://www.slidestalk.com/Baiyulan/20210126OpenData163895?video=&_fuid=40979"
-//             }
-//         ],
-//         turns: [
-//             {
-//                 mmyyyy: "01/2021",
-//                 where: "AI Institute, SJTU, Shanghai",
-//                 note: "<a href='https://mp.weixin.qq.com/s/vVzECjq3ekjh2SxKcqTa3g' target='_blank'>Flyer</a> & <a href='https://mp.weixin.qq.com/s/XmjkcJWKlelpvCu4QLI5zQ' target='_blank'>Press</a> in Chinese"
-//             }
-//         ]
-//     },
-//     {
-//         title: "Towards Trustworthy Medical 3D Vision",
-//         materials: [
-//             {
-//                 tag: "slides",
-//                 href: "https://docs.qq.com/pdf/DS3FaQnRialJBWGl5"
-//             }
-//         ],
-//         turns: [
-//             {
-//                 mmyyyy: "12/2020",
-//                 where: "CV/CG/ML Reading Group (online)",
-//                 note: "Host: <a href='http://www-personal.umich.edu/~zzhengxi/' target='_blank'>Zhengxia Zou</a>"
-//             }
-//         ]
-//     },
-//     {
-//         title: "迈向可信的医学3D视觉：研究方法与感悟",
-//         turns: [
-//             {
-//                 mmyyyy: "11/2020",
-//                 where: "第四届上海交通大学&复旦大学联合电子信息技术学术论坛开幕式 (SJTU, Shanghai)"
-//             }
-//         ]
-//     },
-//     {
-//         title: "Review of MICCAI 2020 RibFrac Challenge",
-//         materials: [
-//             {
-//                 tag: "videos & slides",
-//                 href: "https://ribfrac.grand-challenge.org/program/"
-//             }
-//         ],
-//         turns: [
-//             {
-//                 mmyyyy: "10/2020",
-//                 where: "<a href='https://ribfrac.grand-challenge.org/' target='_blank'>MICCAI 2020 Satellite Event (RibFrac Challenge)</a>"
-//             }
-//         ]
-//     },
-//     {
-//         title: "Tutorial: Techniques, Tricks and Trends in Deep Learning",
-//         materials: [
-//             {
-//                 tag: "slides",
-//                 href: "https://docs.qq.com/pdf/DS09HV21tVGxXQ2NO"
-//             }
-//         ],
-//         turns: [
-//             {
-//                 mmyyyy: "04/2020",
-//                 where: "SJTU Course: EE228 Machine Learning for AI Class"
-//             }
-//         ]
-//     },
-//     {
-//         title: "Tutorial: Design of Modern CNNs",
-//         materials: [
-//             {
-//                 tag: "slides",
-//                 href: "https://docs.qq.com/pdf/DS3ZsVUtVWW1uUGl2"
-//             }
-//         ],
-//         turns: [
-//             {
-//                 mmyyyy: "10/2019",
-//                 where: "SJTU Course: EE369 Machine Learning"
-//             }
-//         ]
-//     },
-//     {
-//         title: "3D视觉遇上医学图像计算",
-//         turns: [
-//             {
-//                 mmyyyy: "08/2019",
-//                 where: "上海交通大学人工智能研究生学术论坛 (SJTU, Shanghai)"
-//             }
-//         ]
-//     },
-//     {
-//         title: "3rd-Place Solution for Alibaba Medical AI Competition",
-//         materials: [
-//             {
-//                 tag: "slides",
-//                 href: "https://docs.qq.com/pdf/DS2xWUFdiZWdWdVlE"
-//             }
-//         ],
-//         turns: [
-//             {
-//                 mmyyyy: "10/2017",
-//                 where: "Alibaba Group, Hangzhou"
-//             },
-//             {
-//                 mmyyyy: "10/2017",
-//                 where: "<a href='https://zhuanlan.zhihu.com/p/30346668' target='_blank'>AI Challenger Webinar</a>"
-//             }
-//         ]
-//     }
+    {
+        title: "TBD",
+        materials: [
+            {
+                tag: "slides",
+                href: "https://docs.qq.com/pdf/DS1Bab21waEhtdXNF"
+            },
+            {
+                tag: "video (YouTube)",
+                href: "https://www.nova77.org/recorded-video-for-webinar-slides-for-medmnist-classification-decathlon-and-clinical-ai-by-jiancheng-yang/4068/"
+            }
+        ],
+        turns: [
+            {
+                mmyyyy: "02/2021",
+                where: " Pie & AI by <a href='https://deeplearning.ai/' target='_blank'>DeepLearning.AI</a> (online)",
+                note: "Host: <a href='https://www.eventbrite.com/e/pie-ai-henderson-medmnist-classification-decathlon-clinical-ai-tickets-137316189419#' target='_blank'>Nova77 STEM Workshop</a>"
+            }
+        ]
+    }
+    {
+        title: "How to Develop Open Research Dataset: Examples of Medical Images",
+        materials: [
+            {
+                tag: "slides",
+                href: "https://docs.qq.com/pdf/DS0xDdEVwTkd2RnBW"
+            },
+            {
+                tag: "video (in Chinese)",
+                href: "https://www.slidestalk.com/Baiyulan/20210126OpenData163895?video=&_fuid=40979"
+            }
+        ],
+        turns: [
+            {
+                mmyyyy: "01/2021",
+                where: "AI Institute, SJTU, Shanghai",
+                note: "<a href='https://mp.weixin.qq.com/s/vVzECjq3ekjh2SxKcqTa3g' target='_blank'>Flyer</a> & <a href='https://mp.weixin.qq.com/s/XmjkcJWKlelpvCu4QLI5zQ' target='_blank'>Press</a> in Chinese"
+            }
+        ]
+    },
+    {
+        title: "Towards Trustworthy Medical 3D Vision",
+        materials: [
+            {
+                tag: "slides",
+                href: "https://docs.qq.com/pdf/DS3FaQnRialJBWGl5"
+            }
+        ],
+        turns: [
+            {
+                mmyyyy: "12/2020",
+                where: "CV/CG/ML Reading Group (online)",
+                note: "Host: <a href='http://www-personal.umich.edu/~zzhengxi/' target='_blank'>Zhengxia Zou</a>"
+            }
+        ]
+    },
+    {
+        title: "迈向可信的医学3D视觉：研究方法与感悟",
+        turns: [
+            {
+                mmyyyy: "11/2020",
+                where: "第四届上海交通大学&复旦大学联合电子信息技术学术论坛开幕式 (SJTU, Shanghai)"
+            }
+        ]
+    },
+    {
+        title: "Review of MICCAI 2020 RibFrac Challenge",
+        materials: [
+            {
+                tag: "videos & slides",
+                href: "https://ribfrac.grand-challenge.org/program/"
+            }
+        ],
+        turns: [
+            {
+                mmyyyy: "10/2020",
+                where: "<a href='https://ribfrac.grand-challenge.org/' target='_blank'>MICCAI 2020 Satellite Event (RibFrac Challenge)</a>"
+            }
+        ]
+    },
+    {
+        title: "Tutorial: Techniques, Tricks and Trends in Deep Learning",
+        materials: [
+            {
+                tag: "slides",
+                href: "https://docs.qq.com/pdf/DS09HV21tVGxXQ2NO"
+            }
+        ],
+        turns: [
+            {
+                mmyyyy: "04/2020",
+                where: "SJTU Course: EE228 Machine Learning for AI Class"
+            }
+        ]
+    },
+    {
+        title: "Tutorial: Design of Modern CNNs",
+        materials: [
+            {
+                tag: "slides",
+                href: "https://docs.qq.com/pdf/DS3ZsVUtVWW1uUGl2"
+            }
+        ],
+        turns: [
+            {
+                mmyyyy: "10/2019",
+                where: "SJTU Course: EE369 Machine Learning"
+            }
+        ]
+    },
+    {
+        title: "3D视觉遇上医学图像计算",
+        turns: [
+            {
+                mmyyyy: "08/2019",
+                where: "上海交通大学人工智能研究生学术论坛 (SJTU, Shanghai)"
+            }
+        ]
+    },
+    {
+        title: "3rd-Place Solution for Alibaba Medical AI Competition",
+        materials: [
+            {
+                tag: "slides",
+                href: "https://docs.qq.com/pdf/DS2xWUFdiZWdWdVlE"
+            }
+        ],
+        turns: [
+            {
+                mmyyyy: "10/2017",
+                where: "Alibaba Group, Hangzhou"
+            },
+            {
+                mmyyyy: "10/2017",
+                where: "<a href='https://zhuanlan.zhihu.com/p/30346668' target='_blank'>AI Challenger Webinar</a>"
+            }
+        ]
+    }
 ];
 
 const misc = [

e0b9e63d395aac13d2fa314540100e228382bc01
lichen14 <380120970@qq.com>
2021-09-16 12:33:54 +0800

    Update data.js

--- data.js
+++ data.js
@@ -10,11 +10,11 @@ const site = {
         },
         {
             tag: "Publications",
-            href: "#sectionPublications"
+            href: "#publications"
         },
         {
             tag: "More",
-            href: "#sectionHonors"
+            href: "#honors"
         },
     ],
     relatedProjects: [
